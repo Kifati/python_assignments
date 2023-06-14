d39ea9d6c674dfc1d4e56d7866766bb91a84db5b
Kifati <85504746+Kifati@users.noreply.github.com>
2023-06-14 18:32:02 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter your marks 1: "))
+n2=int(input("Enter your marks 2: "))
+n3=int(input("Enter your marks 3: "))
+avg=(n1+n2+n3)/3
+print("The average of n1,n2,n3 is: ",avg)
+#OR
+print("The average of ",n1," ,",n2," ,",n3," is ",avg)
