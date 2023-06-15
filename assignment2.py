a1f04fa68b46516ee848bbd54f5470539bf0b494
Kifati <85504746+Kifati@users.noreply.github.com>
2023-06-15 19:40:34 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+#Arithmetic operator (addition)
+print("***Arithmetic Operator***")
+a=int(input("Enter number a: "))
+b=int(input("Enter number b: "))
+print("Addition of a and b is: ",a+b)
+#Assignment operator
+print ("***Assignment Operator***")
+a+=1
+print("a+=1 increments a by 1=",a)
+#Comparision operator
+print("***Comparision Operator***")
+print("a>b checks if a is greater than b and returns the result in boolean:",a>b)
+#Logical Operator
+print("***Logical Operator***")
+x=True
+y=False
+print("x and y returns logical and operation between x and y:",x and y)
+#Bitwise Operator
+print("***Bitwise Operator****")
+print("a>>b shifts the bits to the right",a >> b)
+#Identity Operator
+print("***Identity Operator***")
+print("a is b returns true if both variables are the same object:", a is b)
+#Membership Operator
+print("***Membership Operator***")
+s="karan"
+print("a in s returns true if the character is present in the string s=",s,":","a" in s)
+
