1472945274dd9979dbd0cbe0bb184c5963f0fcd6
Kifati <85504746+Kifati@users.noreply.github.com>
2023-06-18 22:48:26 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,22 @@
+#loop manipulation and for with else (prints even and odd numbers till 16)
+for i in range(1,20):
+    if i==15:
+        continue
+    elif i==16:
+        pass
+    elif i==17:
+        break
+    if i%2==0:
+        print("even",i)
+    else:
+        print("odd",i)
+else:
+    print("break used at 17") #this will not be printed as loop breaks at 17
+print("*************************************") 
+#while with else (prints 10 to 1 descending numbers)
+k=10
+while k!=0:
+    print(k)
+    k-=1
+else: 
+    print("break statement not used")
